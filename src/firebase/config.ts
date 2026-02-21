aaf25ebb28cd18a72c446eb5b5220cb1f402adb2
Adnan unwala <adnanunwala47@gmail.com>
2026-02-21 18:16:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4750659626-3fb26",
+  "appId": "1:182442090335:web:e969f841b9fcfeb1af60ea",
+  "apiKey": "AIzaSyAgwLZgXVGVvbT7IKeIimIZ-YK7KsA3pjg",
+  "authDomain": "studio-4750659626-3fb26.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "182442090335"
+};
