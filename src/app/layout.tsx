591d1cf9e4903b7e3c3802c4051bddbc4229e378
Adnan unwala <adnanunwala47@gmail.com>
2026-02-22 16:35:41 +0000

    nope stop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <meta
+          name="google-site-verification"
+          content="PASTE_YOUR_GOOGLE_CODE_HERE"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
